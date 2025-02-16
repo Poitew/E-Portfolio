e9d9c5b3f38bdc3860910244a4dc9c96e695592c
Poitew <tommasoromano123@gmail.com>
2025-02-16 17:14:53 +0100

    fix

--- src/components/blog/blog-posts/posts.tsx
+++ src/components/blog/blog-posts/posts.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from "./Posts.module.css";
+import styles from "./posts.module.css";
 import posts_json from "/src/data/json/posts.json";
 
 function Posts(){
